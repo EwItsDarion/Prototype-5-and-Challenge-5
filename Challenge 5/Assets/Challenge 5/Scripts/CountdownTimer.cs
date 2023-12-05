afeffce1bc06ca75957c5371842d4f5188fa0d36
EwItsDarion <131904023+EwItsDarion@users.noreply.github.com>
2023-12-04 18:06:33 -0600

    accidentaly delete

---
+++ Challenge 5/Assets/Challenge 5/Scripts/CountdownTimer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public float timeRemaining = 60f;
+    public Text countdownText;
+    private GameManagerX gameManager;
+
+
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerX>();
+    }
+    private void Update()
+    {
+
+        if (timeRemaining > 0)
+        {
+
+            timeRemaining -= Time.deltaTime;
+
+
+            int displayTime = Mathf.RoundToInt(timeRemaining);
+
+            countdownText.text = "Time: " + displayTime.ToString();
+
+
+            if (timeRemaining <= 0)
+            {
+
+                gameManager.GameOver();
+            }
+        }
+    }
+
+
+}
